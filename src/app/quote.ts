fb1da988b8c8e39a92b4f815530d5d761356160b
Moringa School <mmasidesiree9@gmail.com>
2019-06-04 17:20:24 +0300

    Auto-generated commit

--- src/app/quote.ts
+++
@@ -1,6 +0,0 @@
-export class Quote {
-    public showDescription:boolean;
-    constructor(public quote:string, public author:string, public name:string, public upVote: number, public downVote: number, public datePosted:Date){
-        this.showDescription=false;
-    }
-}
